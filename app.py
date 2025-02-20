6b8afc7e0ff84755f77df0fef261985a27cd236d
Ganesh-73005 <163154009+Ganesh-73005@users.noreply.github.com>
2025-02-20 23:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def update_building(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render dynamically assigns a PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
